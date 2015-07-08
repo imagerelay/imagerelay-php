67411a1e79393b24dbbf0c0b7f4aa4e6a3810059
travisdmathis <travisdmathis@gmail.com>
2015-07-08 15:57:56 -0400

    updated readme and added all files end points

--- README.md
+++ README.md
@@ -21,6 +21,36 @@ This is still a work in progress and is not complete.  If you would like to cont
 	$ php composer.phar install
 ```
 
+##Usage
+To use the library you only need to have an Image Relay account with proper permissions to complete the API actions. 
 
+#####Authorization with username and password
+```code
+<?php
+	require_once 'vendor/autoload.php';
 
+	$client = ImageRelay\ImageRelayClient::factory(array(
+	    'auth' => 'http',
+	    'username' => 'username',
+	    'password' => 'password',
+	    'app_name' => 'Awesome APP',
+	    'app_contact' => 'http://www.awesomeapp.com'
+	));
+?>
+```
 
+#####Authorization with oAuth
+When authorizing with oauth you will need to use one of the many existing libraries available to retrieve your oauth access token.  The Image Relay API adheres to oauth standars for authentication.
+
+```code
+<?php
+	require_once 'vendor/autoload.php';
+
+	$client = ImageRelay\ImageRelayClient::factory(array(
+	    'auth' => 'oauth',
+	    'token' => '08dfsafd8asdf8asdf90as8df90df8',
+	    'app_name' => 'Awesome APP',
+	    'app_contact' => 'http://www.awesomeapp.com'
+	));
+?>
+```
--- src/ImageRelay/Resources/api.php
+++ src/ImageRelay/Resources/api.php
@@ -3,6 +3,8 @@
         'serviceFullName'       => 'ImageRelay API',
         'serviceAbbreviation'   => 'IRAPI',
         'operations'            => array(
+            /* FILES */
+            /* getFiles */
             'getFiles' => array(
                 'httpMethod' => 'GET',
                 'uri' => 'folders/{folder_id}/files.json',
@@ -15,13 +17,67 @@
                         'required' => true,
                     ),
                     'uploaded_after' => array(
-                        'location' => 'query',
+                        'location' => 'json',
                         'description' => 'Will return files uploaded to designated folder.  Date format should be YYYY-MM-DD HH:MM:SS -- you can leave off the time if you want to start at the beginning of the day.',
                         'type' => 'string',
                         'required' => false,
                     )
                 )
             ),
+            
+            /* getFile */
+            'getFile' => array(
+                'httpMethod' => 'GET',
+                'uri' => 'files/{id}.json',
+                'summary' => 'Get information about a specific file' . PHP_EOL . '[ImageRelay API: Files](https://github.com/imagerelay/api/blob/master/sections/files.md#get-file)',
+                'parameters' => array(
+                    'id' => array(
+                        'location' => 'uri',
+                        'description' => 'File ID',
+                        'type' => 'integer',
+                        'required' => true,
+                    )
+                )
+            ),
+
+            /* Upload File from URL */
+            'uploadFileFromURL' => array(
+                'httpMethod' => 'POST',
+                'uri' => 'files.json',
+                'summary' => 'Upload a file to a specific folder from a URL' . PHP_EOL . '[ImageRelay API: Files](https://github.com/imagerelay/api/blob/master/sections/files.md#upload-file-from-url)',
+                'parameters' => array(
+                    'filename' => array(
+                        'location' => 'json',
+                        'description' => 'Uploaded Filename',
+                        'type' => 'string',
+                        'required' => true,
+                    ),
+                    'folder_id' => array(
+                        'location' => 'json',
+                        'description' => 'Folder ID',
+                        'type' => 'integer',
+                        'required' => true,
+                    ),
+                    'file_type_id' => array(
+                        'location' => 'json',
+                        'description' => 'File Type ID',
+                        'type' => 'integer',
+                        'required' => true,
+                    ),
+                    'terms' => array(
+                        'location' => 'json',
+                        'description' => 'Metaterms',
+                        'type' => array( 'array', 'object'),
+                        'required' => true,
+                    ),
+                    'url' => array(
+                        'location' => 'json',
+                        'description' => 'URL where the file will be downloaded from',
+                        'type' => 'string',
+                        'required' => true,
+                    )
+                )
+            )
         )
     );
 ?>
