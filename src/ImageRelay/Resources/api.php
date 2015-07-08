43cd90d15fe6e4153169bfba92b840c28f608fb3
travisdmathis <travisdmathis@gmail.com>
2015-07-08 14:12:49 -0400

    fixed syntax error in api.php

--- src/ImageRelay/Resources/api.php
+++ src/ImageRelay/Resources/api.php
@@ -5,7 +5,7 @@
         'operations'            => array(
             'getFiles' => array(
                 'httpMethod' => 'GET',
-                'uri' => 'folders/{folder_id}/files.json'
+                'uri' => 'folders/{folder_id}/files.json',
                 'summary' => 'Get files in a specific folder' . PHP_EOL . '[ImageRelay API: Files](https://github.com/imagerelay/api/blob/master/sections/files.md#get-files)',
                 'parameters' => array(
                     'folder_id' => array(
