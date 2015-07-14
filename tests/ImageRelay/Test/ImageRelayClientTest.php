1a5431f31ef58866527083663c68cf3caedd1292
travisdmathis <travisdmathis@gmail.com>
2015-07-14 10:04:09 -0400

    clean up

--- tests/ImageRelay/Test/ImageRelayClientTest.php
+++ tests/ImageRelay/Test/ImageRelayClientTest.php
@@ -1,13 +1,4 @@
 <?php
-/*
-* (c) Netvlies Internetdiensten
-*
-* Richard van den Brand <richard@netvlies.net>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
-
 namespace ImageRelay\Test;
 
 use ImageRelay\ImageRelayClient;
@@ -26,5 +17,5 @@ class ImageRelayClientTest extends \Guzzle\Tests\GuzzleTestCase
 
         $this->assertEquals('https://subdomain.imagerelay.com/api/v2/', $client->getBaseUrl());
     }
-    
+
 }
